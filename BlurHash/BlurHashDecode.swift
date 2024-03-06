74f53a96d61041f161e6f3f25a28b3d3180b679c
kwazzar <fugitiwe@gmail.com>
2024-03-06 17:25:34 +0200

    BlurHash Start

---
+++ BlurHash/BlurHashDecode.swift
@@ -0,0 +1,8 @@
+//
+//  BlurHashDecode.swift
+//  SideMenuTest
+//
+//  Created by Quasar on 06.03.2024.
+//
+
+import Foundation
---
+++ BlurHash/BlurHashEncode.swift
@@ -0,0 +1,8 @@
+//
+//  BlurHashEncode.swift
+//  SideMenuTest
+//
+//  Created by Quasar on 06.03.2024.
+//
+
+import Foundation
